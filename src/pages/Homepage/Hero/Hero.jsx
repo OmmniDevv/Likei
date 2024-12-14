919c86f7e30a312096ac6ac092787338d52f5a2b
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2024-12-14 18:53:37 +0200

    Modified

--- src/pages/Homepage/Hero/Hero.jsx
+++ src/pages/Homepage/Hero/Hero.jsx
@@ -4,7 +4,24 @@ import styles from "./HeroStyles.module.css";
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <h1>Hero</h1>
+      <div className={styles.part1}>
+        <div className={styles.asidetext}>
+          <h1>a</h1>
+        </div>
+        <div className={styles.maincard}>
+          <h1>maincard1</h1>
+          <h1>maincard1</h1>
+        </div>
+      </div>
+      <div className={styles.text}>
+        <h1>Juli & Alex</h1>
+      </div>
+      <div className={styles.part2}>
+        <div className={styles.video}>vid</div>
+        <div className={styles.box1}>box1</div>
+        <div className={styles.box2}>box2</div>
+        <div className={styles.box3}>box3</div>
+      </div>
     </section>
   );
 };
