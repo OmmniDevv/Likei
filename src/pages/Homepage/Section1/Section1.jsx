bf6fbb7848f6ac3ae47588ee6a49cdbe172e0c86
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2024-12-17 00:30:20 +0200

    Modified

--- src/pages/Homepage/Section1/Section1.jsx
+++ src/pages/Homepage/Section1/Section1.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { useEffect } from "react";
 import styles from "./Section1Styles.module.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Section1 = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <section className={styles.section1}>
-      <h1>Our Moments</h1>
+      <h1
+        data-aos="fade-right"
+        data-aos-delay="200"
+        className={styles.section1title}
+      >
+        Memories
+      </h1>
+      <div className={styles.memories}>
+        <div className={styles.memorycard}>123</div>
+        <div className={styles.memorycard}>123</div>
+        <div className={styles.memorycard}>123</div>
+        <div className={styles.memorycard}>123</div>
+        <div className={styles.memorycard}>123</div>
+        <div className={styles.memorycard}>123</div>
+      </div>
     </section>
   );
 };
