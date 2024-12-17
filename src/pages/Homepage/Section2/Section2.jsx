0ae4b9bbab29f1338f1d35c57f35a2aed57e4b7b
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2024-12-17 22:05:05 +0200

    Modified

--- src/pages/Homepage/Section2/Section2.jsx
+++ src/pages/Homepage/Section2/Section2.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
 import styles from "./Section2Styles.module.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Section2 = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
-    <section className={styles.section2}>
-      <h1>Section2</h1>
-    </section>
+    <div>
+      <section className={styles.section2}>
+        <div data-aos="fade-up" className={styles.section2p1}>
+          <h1 className={styles.section2title}>
+            Thank you<br></br> for <br></br>showing me what love is
+          </h1>
+        </div>
+      </section>
+      <footer className={styles.homepagefooter}>
+        <p>All rights reserved &copy; Julex 2024</p>
+      </footer>
+    </div>
   );
 };
 
