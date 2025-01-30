bcb1d50cc17c53b62d75f1d49311810c2a6fc63a
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-01-30 23:34:00 +0200

    Modified

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Loader from "../pages/Loader/Loader";
 import Header from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
 import styles from "./MainLayoutStyles.module.css";
@@ -9,11 +9,23 @@ const MainLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
 
+  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
   return (
     <div className={styles.layout}>
       <Header isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <Outlet />
+      {loading ? <Loader /> : <Outlet />}
     </div>
   );
 };
