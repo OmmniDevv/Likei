fbcf9e9949e579fb19b3173884437744d65d56c2
OmmniDevv <gyooblup@gmail.com>
2025-04-07 02:07:56 +0700

    Update Homepage.jsx

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
-import styles from "./HomepageStyles.module.css";
+import styles from "./HomePageStyles.module.css";
 import Hero from "../../pages/Homepage/Hero/Hero";
 import Section1 from "../../pages/Homepage/Section1/Section1";
 import Section2 from "../../pages/Homepage/Section2/Section2";
